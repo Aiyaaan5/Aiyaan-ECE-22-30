21069ca65a8c1afa142cb51fd83c8995906ae20c
Aiyaaan5 <139612225+Aiyaaan5@users.noreply.github.com>
2023-09-19 14:59:10 +0530

    Update Program to Calculate tax on your income.c

--- Program to Calculate tax on your income.c
+++ Program to Calculate tax on your income.c
@@ -10,10 +10,10 @@ int main ()
 if (i<=150000)
    	{printf("The tax amount on your income is 0");
    	}   
-else if (i>150000;i<=300000)
+else if (i>150000 && i<=300000)
    	{printf("The tax amount on your income is %d",i/10);
    	}
-else if (i>300000;i<=500000)
+else if (i>300000 && i<=500000)
 	{printf("the tax amount on your income is %d",i*2/10);
 	}
 else if (i>500000)
